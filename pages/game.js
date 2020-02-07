7c81c9d96de2d758cdad979db83b4c43239a1fd7
Ryan Callahan <RyanCallahan312@gmail.com>
2020-02-07 12:33:14 -0500

    secure conn

--- pages/game.js
+++ pages/game.js
@@ -78,7 +78,7 @@ const createListeners = (
 };
 
 const connectSocket = () => {
-    const endpoint = "https://losing-the-lyrics.herokuapp.com:43020";
+    const endpoint = "ws://losing-the-lyrics.herokuapp.com:43020/";
     return io.connect(endpoint, { secure: true });
 };
 
--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const socketServer = require("http").Server(socketApp);
 const io = require("socket.io")(socketServer);
 
 socketServer.listen(43020, () => {
-    console.log("listening on 443");
+    console.log("listening on 43020");
 });
 
 //room class
