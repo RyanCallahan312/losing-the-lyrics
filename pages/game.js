4b149eacdbf9dab629bf486cadf9377a28f3cb4f
Ryan Callahan <RyanCallahan312@gmail.com>
2020-02-09 00:18:53 -0500

    heroku

--- pages/game.js
+++ pages/game.js
@@ -78,7 +78,7 @@ const createListeners = (
 };
 
 const connectSocket = () => {
-    endpoint = "http://losing-the-lyrics.herokuapp.com:43020";
+    const endpoint = "http://losing-the-lyrics.herokuapp.com:43020";
 
     return io(endpoint);
 };
