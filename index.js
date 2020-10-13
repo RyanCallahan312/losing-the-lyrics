8b684b3d603a3aaa312e0ea2ba8130223ffe8d23
Ryan Callahan <RyanCallahan312@gmail.com>
2020-10-13 10:37:43 -0400

    oh should probably also use a relative path

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== 'production';
 
 app.get('/.well-known/acme-challenge/PHZBpY7iF01e21qaAtWWm95fs-rHC5gZZwXgmabdXNQ', (req, res) => {
-    res.sendFile('F:/Personal Docs/Repos/losing-the-lyrics/PHZBpY7iF01e21qaAtWWm95fs-rHC5gZZwXgmabdXNQ.HU1-jb_UpUGyy13Fpr6LUtkl0nCYWYokLdbgqB0H1uQ.txt');
+    res.sendFile(__dirname + '/PHZBpY7iF01e21qaAtWWm95fs-rHC5gZZwXgmabdXNQ.HU1-jb_UpUGyy13Fpr6LUtkl0nCYWYokLdbgqB0H1uQ.txt');
 });
 
 app.listen(port, () => {
